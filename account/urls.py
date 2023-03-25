6c66972e85575e580df9112e3c57d0a311540fc7
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2023-03-24 20:51:59 -0700

    added reset files

--- account/urls.py
+++ account/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name='logout'),
     path("", views.dashboard, name='dashboard'),
+    
+    # password change
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    
+    # password reset
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
